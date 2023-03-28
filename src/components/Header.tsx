369dfbfa3ddcd237bfb5e037635ba31c5c9c11f5
david-franca-algetec <117481634+david-franca-algetec@users.noreply.github.com>
2023-03-28 17:39:44 -0300

    feat(): add logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
 import { Box, Flex, Heading } from "@chakra-ui/react";
+import Image from "next/image";
 import { forwardRef } from "react";
-
+import logo from "../../public/logo.png";
 export const Header = forwardRef<HTMLDivElement>((_, forwardedRef) => (
   <Box as="section" ref={forwardedRef}>
     <Box as="nav" bg="gray.900" w="full">
       <Flex
-        py={{ base: 4, lg: 5 }}
         px={{ base: 4, lg: 8 }}
         justifyContent={{ base: "center", lg: "start" }}
       >
-        <Heading as="h3" size="lg" color="gray.50">
-          Logo
-        </Heading>
+        <Image src={logo} alt="Bike Point Logo" height={64} />
       </Flex>
     </Box>
   </Box>
