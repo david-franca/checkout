81425d96b02a0228203b67c7e8a02348e8ed0165
david-franca-algetec <117481634+david-franca-algetec@users.noreply.github.com>
2023-03-29 09:21:04 -0300

    remove link on head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,15 +2,12 @@ import NextHead from "next/head";
 
 interface HeadType {
   title: string;
-  href?: string;
-  rel?: string;
 }
-export const Head: React.FC<HeadType> = ({ title, href, rel }) => {
+export const Head: React.FC<HeadType> = ({ title }) => {
   return (
     <NextHead>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
-      <link href={href} rel={rel}></link>
     </NextHead>
   );
 };
